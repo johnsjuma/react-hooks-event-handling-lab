cd160af14e38ef981b74d15db495664907d5bf66
jerryjohns <jerry.johns@student.moringaschool.com>
2022-07-18 21:34:01 +0300

    commit tree 6df961008dbbfdd9ce6fffb9292c69125684c604

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+
+import React from "react";
+// Code EyesOnMe Component Here	
+
+const EyesOnMe = () => {
+    const setFocus = () => console.log("Good!");
+    const setblur = () => console.log("Hey! Eyes on me!");
+
+    return <button onFocus={setFocus} onBlur={setblur}>Eyes on me</button>
+}
+
+
+export default EyesOnMe; 
